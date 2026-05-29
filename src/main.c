fe00391bcf548544c648d49cb4a5a11675aa2db0
Ashley Scott <lunarmeadow@protonmail.com>
2026-05-29 11:49:06 -0400

    main.c: raygui skeleton

---
+++ src/main.c
@@ -0,0 +1,39 @@
+/*
+ *  Copyright (C) 2026 lunarmeadow (she/her)
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 3
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "raylib.h"
+
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
+#include "style_lavanda.h"
+
+int main()
+{
+    InitWindow(1280, 720, "lunartiles");
+    SetTargetFPS(60);
+
+    GuiLoadStyleLavanda();
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
